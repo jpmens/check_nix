67e50f10194f234d7362556df5e0ff85334816c0
Jan-Piet Mens <jpmens@gmail.com>
2010-10-15 10:00:42 +0200

    Trivial example of how to use the --statusfile

---
+++ nixstat.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#(@)nixstat.sh
+#
+# Trivial example of how to query the --statusfile created by check_nix
+
+STATUSFILE=/tmp/nix.stat
+
+[ -r $STATUSFILE ] || { echo "$0: Can't open $STATUSFILE" >&2; exit 1; }
+
+STATUS="`head -1 $STATUSFILE`"
+
+case "$STATUS" in
+	OK)
+		echo "--- all ok"
+		;;
+	WARNING)
+		echo "--- oopsie"
+		;;
+	CRITICAL)
+		echo "--- oh, oh!"
+		;;
+	*)
+		echo "$0: don't grok content of $STATUSFILE";
+		exit 2;;
+esac
